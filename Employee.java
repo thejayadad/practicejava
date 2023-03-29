adb3975ae8dbb69827787b9591a6e30927bee911
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-29 10:52:19 -0400

    update

---
+++ Employee.java
@@ -0,0 +1,26 @@
+public class Employee {
+
+    private int employeeId;
+    private String name;
+    private int yearStarted;
+
+    public Employee(){}
+
+
+    public Employee(int employeeId, String name, int yearStarted) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.yearStarted = yearStarted;
+    }
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public String toString(){
+        return "%d %-8s %d".formatted(employeeId, name, yearStarted);
+    }
+
+
+
+}
